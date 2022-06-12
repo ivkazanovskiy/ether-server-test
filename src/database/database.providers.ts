d2b303d64932b56e8c8b33242c59b2c34b2bed70
Ivan Kazanovskiy <ivan.kazanovskiy@foam.agency>
2022-06-12 19:02:53 +0300

    Add database config file

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,17 +1,23 @@
 import { Sequelize } from 'sequelize-typescript';
 import { EtherTransaction } from './etherTransaction.entity';
+import * as sequelizeConfig from '../../config/database.json';
+import { SequelizeConfig } from './types';
+
+console.log(sequelizeConfig);
+
+const config = (
+  process.env.NODE_ENV === 'production'
+    ? sequelizeConfig.production
+    : process.env.NODE_ENV === 'test'
+    ? sequelizeConfig.test
+    : sequelizeConfig.development
+) as SequelizeConfig;
 
 export const databaseProviders = [
   {
     provide: 'SEQUELIZE',
     useFactory: async () => {
-      const sequelize = new Sequelize({
-        dialect: 'postgres',
-        host: '127.0.0.1',
-        username: 'postgres',
-        password: 'postgres',
-        database: 'ether',
-      });
+      const sequelize = new Sequelize(config);
       sequelize.addModels([EtherTransaction]);
       await sequelize.sync();
       return sequelize;
--- src/database/types.ts
+++ src/database/types.ts
@@ -1,3 +1,12 @@
+import { Dialect } from 'sequelize/types';
+
 export enum ERepo {
   ETHER_TRANSACTION = 'ETHER_TRANSACTION',
 }
+export type SequelizeConfig = {
+  username: string;
+  password: string;
+  database: string;
+  host: string;
+  dialect: Dialect;
+};
